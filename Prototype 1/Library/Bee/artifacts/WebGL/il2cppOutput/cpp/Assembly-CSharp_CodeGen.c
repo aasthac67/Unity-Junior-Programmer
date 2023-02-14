8f42545c9ef1703e7a0faf14c80c761e355d1178
Aastha Chowdhary <aasthac@usc.edu>
2023-02-14 12:35:35 -0800

    Junior programming week 1

---
+++ Prototype 1/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,77 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void FollowPlayer::Start()
+extern void FollowPlayer_Start_m138697E0740DB2570074B2DB69AE32AF46A994F9 (void);
+// 0x00000002 System.Void FollowPlayer::LateUpdate()
+extern void FollowPlayer_LateUpdate_m69B79F77CFC8072526CE0BA2D71179F66AE15B70 (void);
+// 0x00000003 System.Void FollowPlayer::.ctor()
+extern void FollowPlayer__ctor_mFEF642F6D296A2FBA30A01B637B6BD8DD2446114 (void);
+// 0x00000004 System.Void MoveObstacle::Start()
+extern void MoveObstacle_Start_mC41AE5392154C4C7AAFFC76ECD01094D342A9763 (void);
+// 0x00000005 System.Void MoveObstacle::Update()
+extern void MoveObstacle_Update_m567F7392A1CC85DA454CC363B4CAB9369E663D90 (void);
+// 0x00000006 System.Void MoveObstacle::.ctor()
+extern void MoveObstacle__ctor_m549EEB8FD97D4E8944C9770E6F44DBD07FC04369 (void);
+// 0x00000007 System.Void PlayerController::Start()
+extern void PlayerController_Start_m1D83076E8B136A71051F2F02545EE04947D3A8CF (void);
+// 0x00000008 System.Void PlayerController::Update()
+extern void PlayerController_Update_m1F4051EB5BCBCCE5EEE2E3E49B7E278C3B14EC33 (void);
+// 0x00000009 System.Void PlayerController::.ctor()
+extern void PlayerController__ctor_mDDAB7C7D82E1A5B3E6C197B1AB9D653DFE554F33 (void);
+static Il2CppMethodPointer s_methodPointers[9] = 
+{
+	FollowPlayer_Start_m138697E0740DB2570074B2DB69AE32AF46A994F9,
+	FollowPlayer_LateUpdate_m69B79F77CFC8072526CE0BA2D71179F66AE15B70,
+	FollowPlayer__ctor_mFEF642F6D296A2FBA30A01B637B6BD8DD2446114,
+	MoveObstacle_Start_mC41AE5392154C4C7AAFFC76ECD01094D342A9763,
+	MoveObstacle_Update_m567F7392A1CC85DA454CC363B4CAB9369E663D90,
+	MoveObstacle__ctor_m549EEB8FD97D4E8944C9770E6F44DBD07FC04369,
+	PlayerController_Start_m1D83076E8B136A71051F2F02545EE04947D3A8CF,
+	PlayerController_Update_m1F4051EB5BCBCCE5EEE2E3E49B7E278C3B14EC33,
+	PlayerController__ctor_mDDAB7C7D82E1A5B3E6C197B1AB9D653DFE554F33,
+};
+static const int32_t s_InvokerIndices[9] = 
+{
+	984,
+	984,
+	984,
+	984,
+	984,
+	984,
+	984,
+	984,
+	984,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	9,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
